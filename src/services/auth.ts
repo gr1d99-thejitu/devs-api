31a6500c095c1e391362f28e35670a6b4eb56911
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2023-06-16 06:53:54 +0300

    chore: add refresh tokens

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -12,7 +12,7 @@ class AuthService {
   static async isPasswordMatch(hashed: string, raw: string): Promise<boolean> {
     return bcrypt.compare(raw, hashed)
   }
-  static async create(credentials: AuthCredentials): Promise<string | null> {
+  static async create(credentials: AuthCredentials): Promise<{ access_token: string; refresh_token: string } | null> {
     const instance = new UserRepository()
     const userRepository = await instance.user()
     const user = await userRepository.findOne({
@@ -32,15 +32,29 @@ class AuthService {
       return null
     }
 
-    return this.generateJwt(user)
+    return {
+      access_token: await this.generateJwt(user),
+      refresh_token: await this.generateRefresh(user)
+    }
   }
 
   static async generateJwt(user: User): Promise<string> {
     const secret = process.env.APP_SECRET_KEY
-    const exp = Math.floor(Date.now() / 1000) + 60 * 60 // 1Hour
+    const exp = 60 // Seconds
     const iat = Math.floor(Date.now() / 1000) - 1 // Backdate 1s
-    return jsonwebtoken.sign({ identity: user.email, exp, iat }, secret as string, {
-      algorithm: 'HS512'
+    return jsonwebtoken.sign({ identity: user.id, iat, kind: 'access' }, secret as string, {
+      algorithm: 'HS512',
+      expiresIn: exp
+    })
+  }
+
+  static async generateRefresh(user: User): Promise<string> {
+    const secret = process.env.APP_SECRET_KEY
+    const exp = 60 * 2 // Seconds = 120 = 2 minutes
+    const iat = Math.floor(Date.now() / 1000) - 1 // Backdate 1s
+    return jsonwebtoken.sign({ identity: user.id, iat, kind: 'refresh' }, secret as string, {
+      algorithm: 'HS512',
+      expiresIn: exp
     })
   }
 }

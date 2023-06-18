358f90b6c6c2cdae9391631e1e5365edb2859e86
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2023-06-18 13:55:44 +0300

    feat: update developer

--- src/api/v1/controllers/developers.ts
+++ src/api/v1/controllers/developers.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { BAD_REQUEST, CREATED, OK } from 'http-status'
+import { BAD_REQUEST, CREATED, NOT_FOUND, OK } from 'http-status'
 import { QueryFailedError } from 'typeorm'
 import { developerRepository } from '../../../repositories/developer'
 
@@ -9,6 +9,28 @@ class DevelopersController {
       const draftDeveloper = developerRepository.create(req.body)
       const developer = await developerRepository.save(draftDeveloper)
       res.status(CREATED).send(developer)
+    } catch (e: any) {
+      if (e instanceof QueryFailedError) {
+        res.status(BAD_REQUEST).send({ errors: e.driverError ?? JSON.stringify(e) })
+        return
+      }
+
+      res.status(BAD_REQUEST).send({ errors: e.message ?? JSON.stringify(e) })
+    }
+  }
+
+  async update(req: express.Request, res: express.Response) {
+    try {
+      const developer = await developerRepository.findOne({ where: { id: req.params['id'] } })
+      if (developer === null) {
+        res.status(NOT_FOUND).send({ errors: { message: 'Record not found!' } })
+        return
+      }
+
+      await developerRepository.save({ ...developer, ...req.body })
+      await developer.reload()
+
+      res.status(OK).send(developer)
     } catch (e: any) {
       if (e instanceof QueryFailedError) {
         res.status(BAD_REQUEST).send({ errors: e.driverError ?? JSON.stringify(e) })
@@ -33,4 +55,4 @@ class DevelopersController {
   }
 }
 
-export default new DevelopersController()
+export const developersController = new DevelopersController()
